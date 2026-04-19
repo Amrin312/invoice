f3a4baede3c169bfec412b580af35d69314a47a0
Amrin <amrinfathimaaf321@gmail.com>
2026-04-19 16:25:07 +0530

    second commit

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from  'cors';
 import 'dotenv/config';
-import { clerkMiddleware } from '@clerk/express'
+
 import { connectDB } from './config/db.js';
 
 
@@ -9,7 +9,7 @@ const app = express();
 const port = 4000;
 
 app.use(cors());
-app.use(clerkMiddleware());
+
 app.use(express.json({limit: "20mb"}));
 app.use(express.urlencoded({limit: "20mb", extended: true}));
 
